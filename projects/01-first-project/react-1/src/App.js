cf4e508be6a2db0e24e8c0dd895292a58750672e
mikhailir <saburov1985@gmail.com>
2023-04-26 23:22:32 +1100

    Вынес компоненты в отдельные файлы

--- projects/01-first-project/react-1/src/App.js
+++ projects/01-first-project/react-1/src/App.js
@@ -1,33 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
+import Technology from './Technologies';
+import Header from './Header';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Technology />
-    </div>
-  );
-};
-
-const Technology = () => {
-  return (
-    <div>
-      <ul>
-        <li>html</li>
-        <li>js</li>
-        <li>css</li>
-      </ul>
-    </div>
-  );
-};
-
-const Header = () => {
-  return (
-    <div>
-      <a href="#1">Link-1</a>
-      <a href="#2">Link-2</a>
-      <a href="#3">Link-3</a>
     </div>
   );
 };
---
+++ projects/01-first-project/react-1/src/Technologies.js
@@ -0,0 +1,13 @@
+const Technology = () => {
+  return (
+    <div>
+      <ul>
+        <li>html</li>
+        <li>js</li>
+        <li>css</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Technology;
