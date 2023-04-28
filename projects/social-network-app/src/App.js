b912356e3e50f3ed27c0729bebe1d6d6fddc6c4c
mikhailir <saburov1985@gmail.com>
2023-04-28 12:59:31 +1100

    Сделал симантическую разбивку страницы с помощью grid

--- projects/social-network-app/src/App.js
+++ projects/social-network-app/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <header className="header"> Header </header>
+      <aside className="aside">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </aside>
+      <main className="main"></main>
     </div>
   );
 }
