8ff415c3ae883b175bc1107c1063d08bed31cf50
mikhailir <saburov1985@gmail.com>
2023-04-30 21:01:24 +1100

    Сделал Main отделной компонентой

--- projects/social-network-app/src/App.js
+++ projects/social-network-app/src/App.js
@@ -1,13 +1,14 @@
 import Header from './components/Header.jsx';
 import './App.css';
 import Nav from './components/Nav.jsx';
+import Main from './components/Main.jsx';
 
 function App() {
   return (
     <div className="app-wrapper">
       <Header />
       <Nav />
-      <main className="main"></main>
+      <Main />
     </div>
   );
 }
--- projects/social-network-app/src/components/Header.jsx
+++ projects/social-network-app/src/components/Header.jsx
@@ -1,5 +1,12 @@
 const Header = () => {
-  return <header className="header"> Header </header>;
+  return (
+    <header className="header">
+      <img
+        src="https://e7.pngegg.com/pngimages/988/459/png-clipart-computer-icons-social-media-social-networking-service-facebook-mobile-social-network-computer-network-social-media-thumbnail.png"
+        alt=""
+      />
+    </header>
+  );
 };
 
 export default Header;
