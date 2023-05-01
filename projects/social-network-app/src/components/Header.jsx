5ed371cf0f1e23c577e93d5ff6ff0651c3a6508a
mikhailir <saburov1985@gmail.com>
2023-05-01 23:10:19 +1100

    создал модуль для Header

--- projects/social-network-app/src/components/Header.jsx
+++ projects/social-network-app/src/components/Header.jsx
@@ -1,8 +1,8 @@
-import './Header.css';
+import s from './Header.module.css';
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className={s.header}>
       <img
         src="https://e7.pngegg.com/pngimages/988/459/png-clipart-computer-icons-social-media-social-networking-service-facebook-mobile-social-network-computer-network-social-media-thumbnail.png"
         alt=""
