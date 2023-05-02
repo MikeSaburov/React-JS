1fc4dddceb1716f2bd1b53bdefd77c9e6afa73f7
mikhailir <saburov1985@gmail.com>
2023-05-02 22:35:37 +1100

    Вынес в отдельную компоненту посты из компоненты Main

--- projects/social-network-app/src/components/Main/Main.jsx
+++ projects/social-network-app/src/components/Main/Main.jsx
@@ -1,7 +1,12 @@
-import s from '../Main.module.css';
+import s from './Main.module.css';
+import MyPosts from './MyPosts/MyPosts';
 
 const Main = () => {
-  return <main className={s.main}></main>;
+  return (
+    <main className={s.main}>
+      <MyPosts />
+    </main>
+  );
 };
 
 export default Main;
