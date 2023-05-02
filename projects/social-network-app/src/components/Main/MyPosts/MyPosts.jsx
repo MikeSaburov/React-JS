33b4606f088d212354e92f0088ea967af430c589
mikhailir <saburov1985@gmail.com>
2023-05-02 22:46:22 +1100

    Набросал один пост для черновика

--- projects/social-network-app/src/components/Main/MyPosts/MyPosts.jsx
+++ projects/social-network-app/src/components/Main/MyPosts/MyPosts.jsx
@@ -11,7 +11,18 @@ const MyPosts = () => {
         <hr />
       </div>
       <div className={s.posts}>
-        <div className={s.item}>Post 1</div>
+        <div className={s.item}>
+          <img
+            src="https://avatarzo.ru/wp-content/uploads/squid-game-anime.jpg"
+            alt=""
+          />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+            asperiores sequi expedita laborum ex mollitia esse, dolorem minima
+            iusto cumque excepturi nulla error explicabo ratione enim quos,
+            itaque repellendus nemo!
+          </p>
+        </div>
         <div className={s.item}>Post 2</div>
         <div className={s.item}>Post 2</div>
       </div>
