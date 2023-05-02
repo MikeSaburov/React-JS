f03c003a46b82d919c6f62d2f07674ff78ac40d1
mikhailir <saburov1985@gmail.com>
2023-05-02 22:40:20 +1100

    Набросал в постах создание нового поста. Временно

--- projects/social-network-app/src/components/Main/MyPosts/MyPosts.jsx
+++ projects/social-network-app/src/components/Main/MyPosts/MyPosts.jsx
@@ -3,8 +3,13 @@ import s from './MyPosts.module.css';
 const MyPosts = () => {
   return (
     <div>
-      My Posts
-      <div>New post</div>
+      <h2>My Posts</h2>
+      <div className="addPost">
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <br />
+        <button>Add post</button>
+        <hr />
+      </div>
       <div className={s.posts}>
         <div className={s.item}>Post 1</div>
         <div className={s.item}>Post 2</div>
