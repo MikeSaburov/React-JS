f144a6c90e7cdaf9afeeb2c93c4416b982b65309
mikhailir <saburov1985@gmail.com>
2023-05-03 21:19:02 +1100

    Прокинул props на посты

--- projects/social-network-app/src/components/Main/MyPosts/MyPosts.jsx
+++ projects/social-network-app/src/components/Main/MyPosts/MyPosts.jsx
@@ -12,7 +12,16 @@ const MyPosts = () => {
         <hr />
       </div>
       <div className={s.posts}>
-        <Post />
+        <Post
+          message="JavaScript сохранил статус самого популярного языка программирования на GitHub в текущем году. Весь топ-5 языков остался неизменным с прошлого года, но PHP уступил шестую строчку – ее занял C++."
+          image="https://cs2.livemaster.ru/storage/3f/d1/b8020a152d3e02369b20a82e53uh--materialy-dlya-tvorchestva-avatarka.jpg"
+          likeCount="150"
+        />
+        <Post
+          message="Mail.ru Group открыла набор на два бесплатных курса по Android- и iOS- разработке для студентов вузов Москвы и Санкт-Петербурга, сообщает пресс-служба компании."
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeyGzxYWrj2oU2vzckVxaD2IBCfvkzDh59XeQkf4RKHnUtbyAc-y789T3l8KKrTMhYfQs&usqp=CAU"
+          likeCount="50"
+        />
       </div>
     </div>
   );
--- projects/social-network-app/src/components/Main/MyPosts/Post/Post.jsx
+++ projects/social-network-app/src/components/Main/MyPosts/Post/Post.jsx
@@ -1,18 +1,11 @@
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
   return (
     <div className={s.item}>
-      <img
-        src="https://avatarzo.ru/wp-content/uploads/squid-game-anime.jpg"
-        alt=""
-      />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui asperiores
-        sequi expedita laborum ex mollitia esse, dolorem minima iusto cumque
-        excepturi nulla error explicabo ratione enim quos, itaque repellendus
-        nemo!
-      </p>
+      <img src={props.image} alt="" />{' '}
+      <span>{`Likes: ${props.likeCount}`}</span>
+      <p>{props.message}</p>
     </div>
   );
 };
