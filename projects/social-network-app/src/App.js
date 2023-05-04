d4ae8696d66d4d670ba26067ef0025e54e887d2d
mikhailir <saburov1985@gmail.com>
2023-05-04 22:02:38 +1100

    Добавил компоненту Dialogs и переименовал Main в Profile, а также вынес разметку контента в общий css

--- projects/social-network-app/src/App.js
+++ projects/social-network-app/src/App.js
@@ -1,14 +1,18 @@
 import Header from './components/Header/Header';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import Main from './components/Main/Main';
+import Profile from './components/Profile/Profile';
+import Doalogs from './components/Dialogs/Dialogs';
 
 function App() {
   return (
     <div className="app-wrapper">
       <Header />
       <Navbar />
-      <Main />
+      <div className="app-wrapper-content">
+        <Doalogs />
+        {/* <Profile /> */}
+      </div>
     </div>
   );
 }
---
+++ projects/social-network-app/src/components/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import s from './Profile.module.css';
+import MyPosts from './MyPosts/MyPosts';
+
+const Profile = () => {
+  return (
+    <content>
+      <MyPosts />
+    </content>
+  );
+};
+
+export default Profile;
