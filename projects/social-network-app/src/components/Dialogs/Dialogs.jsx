1939b81bd647741c2d565e0eaf4db2f692d871ea
mikhailir <saburov1985@gmail.com>
2023-05-06 23:59:27 +1100

    2 первых автора диалогов сделал кликабельными и повесил ссылки

--- projects/social-network-app/src/components/Dialogs/Dialogs.jsx
+++ projects/social-network-app/src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,17 @@
+import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
 
 const Dialogs = () => {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <div className={`${s.dialog} ${s.active}`}>MIke</div>
-        <div className={s.dialog}>Goga</div>
-        <div className={s.dialog}>Rome</div>
+        <div className={`${s.dialog} ${s.active}`}>
+          <NavLink to="/dialogs/1">MIke</NavLink>{' '}
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/2">Goga</NavLink>
+        </div>
+        <div className={s.dialog}>Roma</div>
         <div className={s.dialog}>Artem</div>
         <div className={s.dialog}>Vadik</div>
       </div>
