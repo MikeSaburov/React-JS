1de651c619a85648638bc1601a4e0882d83cae0e
mikhailir <saburov1985@gmail.com>
2023-05-06 22:35:30 +1100

    Переделал стили css на модульную систему

--- projects/social-network-app/src/components/Dialogs/Dialogs.jsx
+++ projects/social-network-app/src/components/Dialogs/Dialogs.jsx
@@ -2,15 +2,19 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
   return (
-    <div className="dialogs">
-      <div className="dialogs-items">
-        <div className="item">MIke</div>
-        <div className="item">Goga</div>
-        <div className="item">Rome</div>
-        <div className="item">Artem</div>
-        <div className="item">Vadik</div>
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>MIke</div>
+        <div className={s.dialog}>Goga</div>
+        <div className={s.dialog}>Rome</div>
+        <div className={s.dialog}>Artem</div>
+        <div className={s.dialog}>Vadik</div>
       </div>
-      <div className="clas"></div>
+      <div className={s.messagesItems}>
+        <div className={s.messages}>Hi</div>
+        <div className={s.messages}>How are you</div>
+        <div className={s.messages}>This is React baby!</div>
+      </div>
     </div>
   );
 };
