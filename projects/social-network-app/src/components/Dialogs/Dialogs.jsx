9b5ef9ee9a56c76fec1ea6a883fa151f3be71d14
mikhailir <saburov1985@gmail.com>
2023-05-11 21:14:53 +1100

    удалил закомментированный лишний код

--- projects/social-network-app/src/components/Dialogs/Dialogs.jsx
+++ projects/social-network-app/src/components/Dialogs/Dialogs.jsx
@@ -2,30 +2,7 @@ import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
 import s from './Dialogs.module.css';
 
-// let dialogsData = [
-//   { id: '1', name: 'Mike' },
-//   { id: '2', name: 'Goga' },
-//   { id: '3', name: 'Roma' },
-//   { id: '4', name: 'Artem' },
-//   { id: '5', name: 'Vadik' },
-//   { id: '6', name: 'Igor' },
-//   { id: '7', name: 'Ivan' },
-// ];
-
-// let messagesData = [
-//   { id: '1', message: 'Hi props!!!' },
-//   { id: '2', message: 'How are you?' },
-//   { id: '3', message: 'This is React baby!' },
-// ];
-
-// let dialogsElemes = dialogsData.map((d) => (
-//   <DialogItem name={d.name} id={d.id} />
-// ));
-
-// let messageElements = messagesData.map((m) => <Message message={m.message} />);
-
 const Dialogs = (props) => {
-  console.log(props);
   let dialogs = props.dialog;
   let messages = props.message;
 
