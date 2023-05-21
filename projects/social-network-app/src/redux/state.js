e51d70222a715b160aad9a6a5d042584492662ee
mikhailir <saburov1985@gmail.com>
2023-05-21 21:51:23 +1100

    сделал создание поста через render.js (третий игрок)

--- projects/social-network-app/src/redux/state.js
+++ projects/social-network-app/src/redux/state.js
@@ -1,3 +1,5 @@
+import { rerenderEntireTree } from '../render';
+
 let state = {
   profilePage: {
     posts: [
@@ -90,6 +92,7 @@ export let addPost = (valuePost) => {
   };
 
   state.profilePage.posts.push(newPost);
+  rerenderEntireTree(state);
 };
 
 export default state;
--- projects/social-network-app/src/render.js
+++ projects/social-network-app/src/render.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-export let rerenderEntireTree = () => {
+import { addPost } from './redux/state';
+
+export let rerenderEntireTree = (state) => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <React.StrictMode>
@@ -12,8 +13,3 @@ export let rerenderEntireTree = () => {
     </React.StrictMode>
   );
 };
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
