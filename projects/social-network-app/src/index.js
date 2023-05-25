148ae09cc01469d4f69def62eb961097308e4bf2
mikhailir <saburov1985@gmail.com>
2023-05-25 23:21:34 +1100

    Создал объект store и прокинул все в компоненты

--- projects/social-network-app/src/index.js
+++ projects/social-network-app/src/index.js
@@ -1,37 +1,36 @@
 import reportWebVitals from './reportWebVitals';
-import state from './redux/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import {
-  addPost,
-  updateNewPostText,
-  addMessage,
-  updateNewMessageText,
-} from './redux/state';
+// import { BrowserRouter } from 'react-router-dom';
+// import {
+//   addPost,
+//   updateNewPostText,
+//   addMessage,
+//   updateNewMessageText,
+// } from './redux/state';
 
-import { subscribe } from './redux/state';
-
-let rerenderEntireTree = () => {
+// import { subscribe } from './redux/state';
+let rerenderEntireTree = (state) => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <React.StrictMode>
       <App
-        appState={state}
-        addPost={addPost}
-        updateNewPostText={updateNewPostText}
-        addMessage={addMessage}
-        updateNewMessageText={updateNewMessageText}
+        appState={store.getState()}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
+        addMessage={store.addMessage.bind(store)}
+        updateNewMessageText={store.updateNewMessageText.bind(store)}
       />
     </React.StrictMode>
   );
 };
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
