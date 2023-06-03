6180b979261aa66b9a2b144132c84569f3768f3b
mikhailir <saburov1985@gmail.com>
2023-06-03 22:16:08 +1100

    Перенес в dialogs-reducer  все что связано с messages

--- projects/social-network-app/src/redux/dialogs-reducer.js
+++ projects/social-network-app/src/redux/dialogs-reducer.js
@@ -1,3 +1,18 @@
+const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
+const ADD_MESSAGE = 'ADD-MESSAGE';
+
 const dialogsReducer = (state, action) => {
+  if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
+    state.newMessageText = action.newMessage;
+  } else if (action.type === ADD_MESSAGE) {
+    let newMessage = {
+      id: 4,
+      message: state.newMessageText,
+    };
+
+    state.messages.push(newMessage);
+    state.newMessage = '';
+  }
+
   return state;
 };
