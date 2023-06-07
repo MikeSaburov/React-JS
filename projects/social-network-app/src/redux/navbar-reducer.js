e9d14c0c19e7772b34029bba73d82a8550dd7210
mikhailir <saburov1985@gmail.com>
2023-06-07 23:08:05 +1100

    Создал initialState  в редъюсерах

--- projects/social-network-app/src/redux/navbar-reducer.js
+++ projects/social-network-app/src/redux/navbar-reducer.js
@@ -1,4 +1,24 @@
-const navbarReducer = (state, action) => {
+let initialState = {
+  friends: [
+    {
+      id: 1,
+      name: 'Andrew',
+      src: 'https://kartinkived.ru/wp-content/uploads/2021/12/avatarka-dlya-vatsapa-krasivaya-panda.jpg',
+    },
+    {
+      id: 2,
+      name: 'Vanya',
+      src: 'https://shapka-youtube.ru/wp-content/uploads/2020/05/gangsta-skull.jpg',
+    },
+    {
+      id: 3,
+      name: 'Sveta',
+      src: 'https://www.meme-arsenal.com/memes/581e1833e4b43d7fd6d4d672ab64d43c.jpg',
+    },
+  ],
+};
+
+const navbarReducer = (state = initialState, action) => {
   return state;
 };
 
