9844d6212d9a9cd8c009f793a87330dbf2e99ddf
mikhailir <saburov1985@gmail.com>
2023-06-07 22:50:44 +1100

    Добавил коммит для понимания

--- projects/social-network-app/src/redux/redux-store.js
+++ projects/social-network-app/src/redux/redux-store.js
@@ -4,6 +4,7 @@ import dialogsReducer from './dialogs-reducer';
 import navbarReducer from './navbar-reducer';
 
 let redusers = combineReducers({
+  // здесь необходимо понимать что ключ это то что хранится в state, а значение это тот reducer который за этот state отвечает
   pofileReducer,
   dialogsReducer,
   navbarReducer,
